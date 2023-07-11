21f26f6c33dba584a1350a6dae9912b01c96244b
dschetu <dschetu@gmail.com>
2023-07-11 14:26:49 +0530

    Typewriter different functionality

--- dist/script.js
+++ dist/script.js
@@ -122,41 +122,41 @@ document.addEventListener('DOMContentLoaded', function () {
   showCards();
 });
 // Function to check if it's the home page
-function isHomePage() {
-  return window.location.pathname === '/dist/' || window.location.pathname === '/';
-}
-// Home page word dissolving effect
-var word = ['Software Integration', 'Artificial Inteligence', 'Custom Software Solution'];
-var currentIndex = 0;
+// function isHomePage() {
+//   return window.location.pathname === '/dist/' || window.location.pathname === '/';
+// }
+// // Home page word dissolving effect
+// var word = ['Software Integration', 'Artificial Inteligence', 'Custom Software Solution'];
+// var currentIndex = 0;
 
-function displayWord() {
-  var wordDisplay = document.getElementById("word-display");
+// function displayWord() {
+//   var wordDisplay = document.getElementById("word-display");
 
-  // Apply dissolve effect to hide the current word
-  wordDisplay.classList.remove("appear");
-  wordDisplay.classList.add("dissolve");
+//   // Apply dissolve effect to hide the current word
+//   wordDisplay.classList.remove("appear");
+//   wordDisplay.classList.add("dissolve");
 
-  setTimeout(function () {
-    // Update the content with the new word
-    wordDisplay.textContent = word[currentIndex];
+//   setTimeout(function () {
+//     // Update the content with the new word
+//     wordDisplay.textContent = word[currentIndex];
 
-    // Apply appear effect to show the new word
-    wordDisplay.classList.remove("dissolve");
-    wordDisplay.classList.add("appear");
+//     // Apply appear effect to show the new word
+//     wordDisplay.classList.remove("dissolve");
+//     wordDisplay.classList.add("appear");
 
-    currentIndex++;
+//     currentIndex++;
 
-    if (currentIndex >= word.length) {
-      currentIndex = 0;
-    }
+//     if (currentIndex >= word.length) {
+//       currentIndex = 0;
+//     }
 
-    setTimeout(displayWord, 3000);
-  }, 2000);
-}
+//     setTimeout(displayWord, 3000);
+//   }, 2000);
+// }
 
-if (isHomePage()) {
-  displayWord();
-}
+// if (isHomePage()) {
+//   displayWord();
+// }
 
 // Home page worder changer with typewriter effect
 // const words = ['  App Development     ', '  Artificial Intelligence     ', '  Business Intelligence     '];
@@ -189,16 +189,75 @@ if (isHomePage()) {
 
 // type();
 
-// google reCAPTCHA
-grecaptcha.ready(function () {
-  grecaptcha.execute("6LdlG8YmAAAAAHn7IaOetjvTqulE42vgHzpoJWOG")
-    .then(function (token) {
-      document.getElementById('captchaResponse').value = token;
-    });
-});
+// // google reCAPTCHA
+// grecaptcha.ready(function () {
+//   grecaptcha.execute("6LdlG8YmAAAAAHn7IaOetjvTqulE42vgHzpoJWOG")
+//     .then(function (token) {
+//       document.getElementById('captchaResponse').value = token;
+//     });
+// });
 
-function displayFileName(fileInputId, fileNameId) {
-  var fileInput = document.getElementById(fileInputId);
-  var fileNameLabel = document.getElementById(fileNameId);
-  fileNameLabel.textContent = fileInput.files[0].name;
-}
+// function displayFileName(fileInputId, fileNameId) {
+//   var fileInput = document.getElementById(fileInputId);
+//   var fileNameLabel = document.getElementById(fileNameId);
+//   fileNameLabel.textContent = fileInput.files[0].name;
+// }
+
+// TYPEWRITING UPDATED FUNCTIONALITY
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style2");
+  document.css.type = "text/css";
+  document.css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
